4f122b4562a752f343e69fc9db2d3c923329ec6e
aqun2023 <122092067+aqun2023@users.noreply.github.com>
2024-09-07 11:46:49 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   appid: "wxd58c90f4f136a1c2",
   appsecret: "87915c94b6a71c01a49307885c552345",
   template_id: "dpUszAJcetYC1dksKcuitfxq3m9URRngI73-3yHuyts",
-  touser: "o9YWi6U5rtl6PWEzrd3uugVV-3RM",
+  touser: "o9YWi6YujiF6qCEyEj9NSqPMav3M",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "珠海",
